3e155b907328f9555719f105c72e67f484fc753a
Aliaa Adel <35958413+aliaaadel@users.noreply.github.com>
2023-01-20 19:39:24 +0200

    adding comments to create_user

--- create_user.sh
+++ create_user.sh
@@ -1,7 +1,10 @@
+#first check if  user in DB admin
 current=$(whoami)
 isexist=$(cat DB_admins.db | grep -w $current | wc -l)
 if [[ $isexist -eq 1 ]]
 then
+	#get user name to be added to Admin and then checks if it already exists ,if not then 
+	#add the user entered to DB_admins 
 	echo Please, enter the user you want to add as an ADMIN!
 	read adname
 	isexist=$(cat DB_admins.db | grep -w $adname | wc -l)
@@ -16,6 +19,7 @@ then
 else
     echo "You're ($(whoami)) not an admin and don't have that privilage"
 fi
+#return to main.sh
 echo "---------------------------------------"
 echo
 PS3="Please, Choose an option: "
