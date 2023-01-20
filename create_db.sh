390f7db68374d7b05e124c3bff1c44fff3685e84
Aliaa Adel <35958413+aliaaadel@users.noreply.github.com>
2023-01-20 19:25:57 +0200

    adding comments to DB_create

--- create_db.sh
+++ create_db.sh
@@ -1,22 +1,25 @@
+#start to check if the user is an Admin or not 
 current=$(whoami)
 isexist=$(cat DB_admins.db | grep -w $current | wc -l)
 if [[ $isexist -eq 1 ]]
 then
-	# echo "has access"
+	#get DB name from user
 	read -p "Enter DB name: " db_name
 	output=$(ls ./Databases | grep -w $db_name | wc -l)
 	if [[ $output -eq 1 ]] #check if the DB already exists 
 	then
 		echo This database already exists
 	else 
+		#create dir and parent dir if not existent 
 		mkdir -p ./Databases/$db_name 
 		echo "Database Created Successfully "
-		echo $current >> ./Databases/$db_name/owner.txt
+		echo $current >> ./Databases/$db_name/owner.txt # create owner.txt and add user to it 
 	fi
 else
     echo "You're ($(whoami)) not an admin and don't have that privilage"
 fi
 echo "---------------------------------------"
 echo
-PS3="Please, Choose an option: "
-return
+# return to main.sh 
+PS3="Please, Choose an option: " 
+return 
