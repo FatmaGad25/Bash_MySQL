c642b4f4ff133099fc15c61aea18b128f608c022
Aliaa Adel <35958413+aliaaadel@users.noreply.github.com>
2023-01-20 19:32:45 +0200

    adding comments to create_table

--- create_table.sh
+++ create_table.sh
@@ -1,12 +1,13 @@
+#first check if  user check in DB admin 
 PS3="Please, Choose a Database: "
 u=$(whoami)
 isexist=$(cat DB_admins.db | grep -w $u | wc -l)
 if [[ $isexist -eq 1 ]]
 then
-    # echo "Available Databases: "
-    # ls ./Databases 
+    # list available DB and select on of them
     select db in $(ls ./Databases) exit
     do
+    	# return to main.sh if choosing to exit 
         if [[ $db == exit ]]
         then 
             echo "---------------------------------------"
@@ -14,16 +15,17 @@ then
             PS3="Please, Choose an option: "
             return
         else
-
+	    # get owner of DB , if user is the owner then create table if not exit
             owner=$(head -1 ./Databases/$db/owner.txt)
             if [[ $owner == $u ]]
             then
                 read -p "Enter table name: " tbname
-                isexist=$(ls ./Databases/$db/ | grep -w $tbname | wc -l)
-                if [[ $isexist -eq 1 ]]
+                isexist=$(ls ./Databases/$db/ | grep -w $tbname | wc -l) #check if table already exists 
+                if [[ $isexist -eq 1 ]] 
                 then
                     echo "This table already exists in this database"
                 else
+		    #create table and get the number of columns and column names
                     touch ./Databases/$db/$tbname.table
                     read -p "Enter number of columns: " numofco
                     echo "Creating table ($tbname) with $numofco columns ....."
@@ -33,6 +35,7 @@ then
                     do
                         index=$(($index+1))
                         read -p "Enter column $index name: " cname
+			# append column name to header each iteration 
                         if [[ $index -eq $numofco ]]
                         then
                             header=$header$cname
@@ -40,9 +43,11 @@ then
                             header=$header$cname","
                         fi
                     done
+		    #add header to the table
                     echo $header>> ./Databases/$db/$tbname.table
                     echo "Table Created Successfully "
                     echo "---------------------------------------"
+		    # return to main.sh
                     echo
                     PS3="Please, Choose an option: "
                     return
@@ -53,11 +58,13 @@ then
         fi
         echo "---------------------------------------"
         echo
+	#to display select menu again 
         REPLY=
     done
 else
     echo "You're ($(whoami)) not an admin and don't have that privilage"
 fi
+# return to main.sh
 echo "---------------------------------------"
 echo
 PS3="Please, Choose an option: "
