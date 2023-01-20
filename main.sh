7ec20dc4d33c6955b27ea18f3f3a1cc0b5895a97
Aliaa Adel <35958413+aliaaadel@users.noreply.github.com>
2023-01-20 20:44:20 +0200

    adding comments to main

--- main.sh
+++ main.sh
@@ -1,11 +1,13 @@
 PS3="Please, Choose an option: "
+#select from list of options inside txt file
 select option in $(cat ./options.txt) exit
 do
 if [[ $option == exit ]]
 then 
-	return
+	return 
 else
 	. $option
 fi
+#display select menu after selection 
 REPLY=
 done
